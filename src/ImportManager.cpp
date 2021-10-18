6d29d34b519e88551c0543824ac7d4b19f99b80a
Parapets <southwest85b@gmail.com>
2021-10-18 19:02:42 +0100

    Load movies requested by configs

--- src/ImportManager.cpp
+++ src/ImportManager.cpp
@@ -1,5 +1,22 @@
 #include "ImportManager.h"
 
+ImportManager::ImportManager()
+{
+	RE::GFxLoader::LoaderConfig loaderConfig{
+		RE::GFxLoader::kLoadAll,
+		RE::GPtr<RE::GFxFileOpenerBase>{ new RE::BSScaleformFileOpener{} },
+		RE::GPtr<RE::GFxZlibSupportBase>{ new RE::GFxZlibSupport{} },
+		RE::GPtr<RE::GFxJpegSupportBase>{ nullptr },
+	};
+
+	_loader = new RE::GFxLoader{ loaderConfig };
+}
+
+ImportManager::~ImportManager()
+{
+	delete _loader;
+}
+
 auto ImportManager::GetSingleton() -> ImportManager*
 {
 	static ImportManager singleton{};
@@ -13,13 +30,11 @@ auto ImportManager::AddCustomIcon(std::filesystem::path a_source, const std::str
 
 	auto canonical = std::filesystem::canonical(a_source).string();
 
-	//if (!_importedMovies.contains(canonical)) {
-	//	RE::GFxLoader loader{};
-	//	auto movieDef = loader.CreateMovie(canonical.c_str());
-	//	movieDef->AddRef();
+	if (!_importedMovies.contains(canonical)) {
+		auto movieDef = RE::GPtr<RE::GFxMovieDef>{ _loader->CreateMovie(canonical.c_str()) };
 
-	//	_importedMovies[canonical] = movieDef;
-	//}
+		_importedMovies[canonical] = movieDef;
+	}
 
 	return _nextIndex++;
 }
--- src/ImportManager.h
+++ src/ImportManager.h
@@ -5,6 +5,12 @@ class ImportManager
 public:
 	static auto GetSingleton() -> ImportManager*;
 
+	~ImportManager();
+	ImportManager(const ImportManager& other) = delete;
+	ImportManager(ImportManager&& other) = delete;
+	ImportManager& operator=(const ImportManager& other) = delete;
+	ImportManager& operator=(ImportManager&& other) = delete;
+
 	// TODO inject the icons
 
 	auto AddCustomIcon(
@@ -18,12 +24,14 @@ private:
 		std::string ExportName;
 	};
 
-	ImportManager() = default;
+	ImportManager();
 
 	std::uint32_t _lastIndexFromSwf{ 66 };
 	std::uint32_t _nextIndex{ 67 };
 
 	std::vector<IconInfo> _customIcons;
 
-	std::unordered_map<std::string, RE::GFxMovieDef*> _importedMovies;
+	std::unordered_map<std::string, RE::GPtr<RE::GFxMovieDef>> _importedMovies;
+
+	RE::GFxLoader* _loader;
 };
